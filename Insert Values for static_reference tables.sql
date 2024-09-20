ece698f96acce72c5eb3a289d03de19c062f9fba
ikijima <titus.sasmoko@gmail.com>
2024-09-20 19:48:55 +0800

    none

--- Insert Values for static_reference tables.sql
+++ Insert Values for static_reference tables.sql
@@ -7,34 +7,33 @@ INSERT INTO products (product_id, product_name, price, description, stock_level)
 (005, 'Iced Aren Latte',13000,'Espresso with milk and brown sugar syrup',60);
 
 -- Inserting values into raw_materials
-insert into raw_materials (material_name, unit) VALUES
-('Coffee beans','grams'),
-('UHT milk','liters'),
-('Condensed milk','liters'),
-('Ice','kg'),
-('Vanilla Syrup','ml'),
-('Hazelnut Syrup','ml'),
-('Aren syrup','ml');
+insert into raw_materials (raw_mat_id, material_name, unit, stock_level, reorder_threshold) VALUES
+('RM001','Coffee beans','grams',50000, 16200 ),
+('RM011','UHT milk','liters',500, 225),
+('RM002','Ice','kg',270,135),
+('RM021','Vanilla Syrup','ml', 27000, 9000),
+('RM022','Hazelnut Syrup','ml',27000, 9000),
+('RM023','Aren syrup','ml',27000,9000);
 
 -- Inserting values into recipes
 INSERT INTO recipes (product_id, material_id, quantity_required) VALUES
-(1, 1, 18.0),  -- Iced Americano uses 18g of coffee beans
-(1, 4, 0.15),  -- Iced Americano uses 150g of ice
-(2, 1, 18.0),  -- Iced Latte uses 18g of coffee beans
-(2, 2, 0.25),  -- Iced Latte uses 250ml of UHT milk
-(2, 4, 0.15),  -- Iced Latte uses 150g of ice
-(3, 1, 18.0),  -- Iced Vanilla Latte uses 18g of coffee beans
-(3, 2, 0.25),  -- Iced Vanilla Latte uses 250ml of UHT milk
-(3, 5, 10.0),  -- Iced Vanilla Latte uses 10ml of vanilla syrup
-(3, 4, 0.15),  -- Iced Vanilla Latte uses 150g of ice
-(4, 1, 18.0),  -- Iced Hazelnut Latte uses 18g of coffee beans
-(4, 2, 0.25),  -- Iced Hazelnut Latte uses 250ml of UHT milk
-(4, 6, 10.0),  -- Iced Hazelnut Latte uses 10ml of hazelnut syrup
-(4, 4, 0.15),  -- Iced Hazelnut Latte uses 150g of ice
-(5, 1, 18.0),  -- Iced Aren Latte uses 18g of coffee beans
-(5, 2, 0.25),  -- Iced Aren Latte uses 250ml of UHT milk
-(5, 7, 10.0),  -- Iced Aren Latte uses 10ml of aren syrup
-(5, 4, 0.15);  -- Iced Aren Latte uses 150g of ice
+('RC001001', 001, 'RM001', 18.0),  -- Iced Americano uses 18g of coffee beans
+('RC001004', 001, 'RM002', 0.15),  -- Iced Americano uses 150g of ice
+('RC002001', 002, 'RM001', 18.0),  -- Iced Latte uses 18g of coffee beans
+('RC002002', 002, 'RM011', 0.25),  -- Iced Latte uses 250ml of UHT milk
+('RC002002', 002, 'RM004', 0.15),  -- Iced Latte uses 150g of ice
+(003, 1, 18.0),  -- Iced Vanilla Latte uses 18g of coffee beans
+(003, 2, 0.25),  -- Iced Vanilla Latte uses 250ml of UHT milk
+(003, 5, 10.0),  -- Iced Vanilla Latte uses 10ml of vanilla syrup
+(003, 4, 0.15),  -- Iced Vanilla Latte uses 150g of ice
+(004, 1, 18.0),  -- Iced Hazelnut Latte uses 18g of coffee beans
+(004, 2, 0.25),  -- Iced Hazelnut Latte uses 250ml of UHT milk
+(004, 6, 10.0),  -- Iced Hazelnut Latte uses 10ml of hazelnut syrup
+(004, 4, 0.15),  -- Iced Hazelnut Latte uses 150g of ice
+(005, 1, 18.0),  -- Iced Aren Latte uses 18g of coffee beans
+(005, 2, 0.25),  -- Iced Aren Latte uses 250ml of UHT milk
+(005, 7, 10.0),  -- Iced Aren Latte uses 10ml of aren syrup
+(005, 4, 0.15);  -- Iced Aren Latte uses 150g of ice
 
 -- Inserting values into raw materials purchases
 INSERT INTO raw_materials_purchases (material_id, purchase_date, supplier_name, quantity, price_per_unit) VALUES
